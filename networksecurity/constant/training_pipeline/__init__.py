654fe2d71f416d03cfa3e9a8c74fe5e4ce106b58
vwaghmare <vrudishwaghmare9712@gmail.com>
2026-07-20 12:19:40 +0530

    model and artifacts pushed to aws s3 bucket

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -76,3 +76,8 @@ MODEL_TRAINER_TRAINED_MODEL_DIR:str = 'trained_model'
 MODEL_TRAINER_TRAINED_MODEL_NAME:str = 'model.pkl'
 MODEL_TRAINER_EXPECTED_SCORE:float = 0.6
 MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD:float = 0.05
+
+"""
+AWS related constant variable
+""" 
+TRAINING_BUCKET_NAME = 'vrudish-networksecurity'
